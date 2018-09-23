27423573d246b47040bb53c7e913e7da51ceb2ec
Catherine Zeng <yczeng@mit.edu>
2018-09-23 18:14:33 -0400

    small changes to layout

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # word-segmentation
+
+## Data
+There are 56797 utterances, with an average of 3.982217370635773 in each utterance.
--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ def removesStressPhonemes(speech):
 	result = ""
 	for line in speech:
 		#removes stress and phonemes
-		result += line.replace("1", "").replace("0", "").replace("P", "")
+		result += line.replace("1", "").replace("0", "").replace("P", "").replace("\n", "")
 	return result
 
 def separateByWords(speech):
@@ -16,19 +16,32 @@ def separateByWords(speech):
 	Creates an array of an array of utterances separated by words.
 	utterance separated by 'U', words separated by 'W'
 	'''
+	totalLen = 0
+	count = 0
+
 	result = []
 	listOfUtterances = speech.split("U")
+	listOfUtterances = list(filter(None, listOfUtterances))
+
 	for eachUtterance in listOfUtterances:
 		# rempves empty strings
 		tmpList = list(filter(None, eachUtterance.split("W")))
+		
 		result.append(tmpList)
-	return result
+
+		# to return average length of utterances
+		totalLen += len(tmpList)
+		count += 1
+
+	return result, totalLen / count
 
 if __name__ == '__main__':
-	speech_input = 'mother.speech.txt'
+	speech_input = 'data/mother.speech.txt'
 	with open(speech_input, "r") as speech:
-		processedSpeech = separateByWords(removesStressPhonemes(speech))
+		processedSpeech, avgLen = separateByWords(removesStressPhonemes(speech))
 		print(processedSpeech)
+		print(len(processedSpeech))
+		print(avgLen)
 
-		with open('splitByWords.txt','a') as new_speech:
+		with open('data/splitByWords.txt','a') as new_speech:
 			new_speech.write(str(processedSpeech))
